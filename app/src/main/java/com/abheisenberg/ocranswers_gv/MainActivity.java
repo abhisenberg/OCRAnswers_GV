80845d5fcebef900c6c13337fa01e26de4af4811
abhisenberg <ab96sh@gmail.com>
2018-02-20 22:22:00 +0530

    Without taking pic

--- app/src/main/java/com/abheisenberg/ocranswers_gv/MainActivity.java
+++ app/src/main/java/com/abheisenberg/ocranswers_gv/MainActivity.java
@@ -1,13 +1,131 @@
 package com.abheisenberg.ocranswers_gv;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    SurfaceView cameraView;
+    TextView tv_OCRText;
+    CameraSource cameraSource;
+    final int cameraPermissionID = 1001;
+    private final String TAG = "MA";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cameraView = findViewById(R.id.surface_view);
+        tv_OCRText = findViewById(R.id.tv_OCRText);
+        TextRecognizer textRecognizer = new TextRecognizer
+                .Builder(getApplicationContext())
+                .build();
+
+        if (!textRecognizer.isOperational()) {
+            Log.d(TAG, "OCR dependencies not available");
+
+        } else {
+            cameraSource = new CameraSource.Builder(getApplicationContext(), textRecognizer)
+                    .setFacing(CameraSource.CAMERA_FACING_BACK)
+                    .setRequestedPreviewSize(1280, 1024)
+                    .setRequestedFps(2.0f)
+                    .setAutoFocusEnabled(true)
+                    .build();
+
+            cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+                @Override
+                public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                    try {
+                        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                            ActivityCompat.requestPermissions(MainActivity.this,
+                                    new String[] {Manifest.permission.CAMERA},
+                                    cameraPermissionID);
+                            return;
+                        }
+                        Log.d(TAG, "Starting Camera");
+                        cameraSource.start(cameraView.getHolder());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+                }
+
+                @Override
+                public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                    cameraSource.stop();
+                }
+            });
+
+            textRecognizer.setProcessor(new Detector.Processor<TextBlock>() {
+                @Override
+                public void release() {
+
+                }
+
+                @Override
+                public void receiveDetections(Detector.Detections<TextBlock> detections) {
+                    final SparseArray<TextBlock> items = detections.getDetectedItems();
+                    if(items.size() != 0){
+                        tv_OCRText.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                StringBuilder stringBuilder = new StringBuilder();
+                                for(int i=0; i<items.size(); i++){
+                                    stringBuilder.append(items.valueAt(i).getValue())
+                                    .append("\n");
+                                }
+                                tv_OCRText.setText(stringBuilder.toString());
+                            }
+                        });
+                    }
+                }
+            });
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case cameraPermissionID:
+            {
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        try {
+                            Log.d(TAG, "onRequestPermissionsResult: Starting camera ");
+                            cameraSource.start(cameraView.getHolder());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                } else {
+                    Toast.makeText(this, "Please grant Camera permission from settings!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
 }
